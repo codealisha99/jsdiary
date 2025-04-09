24568157f80ab2df36fbbf6c6f836d2063fcdfa2
codealisha99 <codealisha99@gmail.com>
2025-04-09 22:36:44 +0530

    chapter-2-working with varaibles

---
+++ day1.js
@@ -0,0 +1,38 @@
+// how to do async js and what is callback and  what is callback hell
+// setTimeout function use 
+// promises padhna hai
+
+
+// setTimeout( () =>{
+// console.log("hello their")
+// },5000)
+
+// console.log(" you are so \"nice"")
+
+var watzmyname = "javascript is fun";
+console.log(watzmyname);
+console.log(watzmyname.length);
+console.log(watzmyname.toUpperCase());
+console.log(watzmyname.toLowerCase());
+console.log(watzmyname.indexOf("is"));
+console.log(watzmyname.lastIndexOf("is"));
+console.log(watzmyname.slice(0, 10));
+console.log(watzmyname.slice(0, 10).toUpperCase());
+console.log(watzmyname.replace("fun", "awesome"));
+console.log(watzmyname.split(" "));
+console.log(watzmyname.split(" ").reverse());
+console.log(watzmyname.split(" ").reverse().join(" "));
+console.log(watzmyname.split(" ").reverse().join(" ").toUpperCase());
+console.log(watzmyname.split(" ").reverse().join(" ").toUpperCase().replace("AWESOME", "AWESOME!"));
+console.log(watzmyname.split(" ").reverse().join(" ").toUpperCase().replace("AWESOME", "AWESOME!").charAt(0));
+console.log(watzmyname.concat(" is a programming language"));
+console.log(watzmyname.substring(0, 10));
+console.log(watzmyname.substr(0, 10));
+console.log(watzmyname.startsWith("javascript"));
+console.log(watzmyname.endsWith("fun"));
+console.log(watzmyname.includes("is"));
+console.log(watzmyname.includes("fun"));
+console.log(watzmyname.includes("awesome"));
+console.log(watzmyname.includes("awesome", 10));
+console.log(watzmyname.includes("awesome", 20));
+
